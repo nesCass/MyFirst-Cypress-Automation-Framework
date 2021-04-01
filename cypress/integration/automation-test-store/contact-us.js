06ce18974aebc3f2aab4972c3de39f94f8a4ec29
Ivica Nikolic <ivicanklc@gmail.com>
2021-04-01 09:17:05 +0200

    add retry logic

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -9,7 +9,7 @@ describe(`Test 'Contact us' form via Automation Test Store`, () => {
   beforeEach(() => {
     cy.visit("https://automationteststore.com/");
     cy.xpath(
-      "//a[contains(@href,'https://automationteststore.com/index.php?rt=content/contact')]"
+      "//a[contains(@href,'https://automationteststore.com/index.php?rt=content/contact999')]"
     )
       .click()
       .then(function (obj) {
@@ -17,23 +17,34 @@ describe(`Test 'Contact us' form via Automation Test Store`, () => {
       });
   });
 
-  it(`Should be able to submit a successful submission via contact us form`, () => {
-    //koristimo podatke iz json fajla us pomoc aliasa
-    cy.get("@ivica").then((user) => {
-      cy.get("#ContactUsFrm_first_name").type(user.first_name);
-      cy.get("#ContactUsFrm_email").type(user.email);
-    });
+  it(
+    `Should be able to submit a successful submission via contact us form`,
+    {
+      retries: {
+        // Configure retry attempts for `cypress run` (from command line)  // Default is 0
+        runMode: 2,
+        // Configure retry attempts for `cypress open` (from cypress runner)   // Default is 0
+        openMode: 2,
+      },
+    },
+    () => {
+      //koristimo podatke iz json fajla us pomoc aliasa
+      cy.get("@ivica").then((user) => {
+        cy.get("#ContactUsFrm_first_name").type(user.first_name);
+        cy.get("#ContactUsFrm_email").type(user.email);
+      });
 
-    //assert
-    cy.get("#ContactUsFrm_email").should("have.attr", "name", "email"); // da li atribut 'name' ima vrednost 'email'
+      //assert
+      cy.get("#ContactUsFrm_email").should("have.attr", "name", "email"); // da li atribut 'name' ima vrednost 'email'
 
-    cy.get("#ContactUsFrm_enquiry").type("nesto ovde pise");
-    cy.xpath("//button[@title='Submit']").click();
+      cy.get("#ContactUsFrm_enquiry").type("nesto ovde pise");
+      cy.xpath("//button[@title='Submit']").click();
 
-    //assert
-    cy.get(".mb40 > :nth-child(3)").should(
-      "have.text",
-      "Your enquiry has been successfully sent to the store owner!"
-    );
-  });
+      //assert
+      cy.get(".mb40 > :nth-child(3)").should(
+        "have.text",
+        "Your enquiry has been successfully sent to the store owner!"
+      );
+    }
+  );
 });
